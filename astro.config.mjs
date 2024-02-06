83f913c0e0d6e66ababbf69365f9b081bf99e908
Kenny Tye <kennytye.dev@gmail.com>
2024-02-05 19:55:13 -0800

    build: add vercel adapter for ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,20 +4,20 @@ import alpine from "@astrojs/alpinejs";
 import react from "@astrojs/react";
 import { sanityIntegration } from "@sanity/astro";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  integrations: [
-    tailwind(),
-    react(),
-    alpine({ entrypoint: "/src/entrypoint" }),
-    sanityIntegration({
-      projectId: "epj8euse",
-      dataset: "production",
-      // Set useCdn to false if you're building statically.
-      useCdn: false,
-      // Access the Studio on your.url/admin
-      studioBasePath: "/admin",
-    }),
-  ],
+  integrations: [tailwind(), react(), alpine({
+    entrypoint: "/src/entrypoint"
+  }), sanityIntegration({
+    projectId: "epj8euse",
+    dataset: "production",
+    // Set useCdn to false if you're building statically.
+    useCdn: false,
+    // Access the Studio on your.url/admin
+    studioBasePath: "/admin"
+  })],
+  adapter: vercel()
 });
