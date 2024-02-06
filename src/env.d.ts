f8500d2ce2fa8464e28c5ef0411d44238b2fb0ed
Kenny Tye <kennytye.dev@gmail.com>
2024-02-05 19:44:22 -0800

    chore: fix type errors and alpine script directives

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,3 +4,11 @@
 interface Window {
   Alpine: import("alpinejs").Alpine;
 }
+
+declare module "sanity:client" {
+  export const sanityClient: import("@sanity/client").SanityClient;
+}
+
+declare module "sanity:studio" {
+  export const studioConfig: import("sanity").Config;
+}
